3e97a5bb8011e1c0cffd9841ed453c97b8dbbf8f
gijscreator <65440619+gijscreator@users.noreply.github.com>
2025-12-17 08:48:57 +0100

    added select gender, purpose and age #20

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -4,17 +4,31 @@ const passswordSection = document.querySelector('.choosepassword');
 const usernameSection =  document.querySelector('.chooseusername');
 const passwordBtn =  document.querySelector('.passwordstep');
 const usernameBtn =  document.querySelector('.usernamestep');
+const images = document.querySelector('.image-previews');
+const chooseGender = document.querySelector('.choosegender');
+const submitBtn = document.querySelector('.submit');
 
 choosePassword.addEventListener('click', codeBtnHandler);
 
 function codeBtnHandler () {
   codeSection.classList.add("hide")
+  images.classList.add("hide");
   passswordSection.classList.remove("hide");
+  
 }
 
 passwordBtn.addEventListener('click', passwordChosen);
 
 function passwordChosen () {
   passswordSection.classList.add("hide");
+  images.classList.add("hide");
   usernameSection.classList.remove("hide");
 }
+
+usernameBtn.addEventListener('click', usernameChosen);
+
+function usernameChosen () {
+  usernameSection.classList.add("hide");
+  chooseGender.classList.remove("hide"); 
+}
+
